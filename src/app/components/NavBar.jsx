08952572ab012d672a76afba7c91d99c40e0f0cf
roccacr <soporte@roccacr.com>
2024-07-23 11:02:51 -0600

    feat: Add responsive behavior to sidebar in AppLayout component

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,9 +1,211 @@
+import { useEffect } from "react";
+import {
+  Drawer,
 
+  Divider,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+// import { startLogout } from "../../store/auth";
+import {  NavLink } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
 
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "center",
+}));
 
+const NavbarContent = styled("div")({
+  overflowY: "auto",
+  height: "calc(100vh - 64px)",
+  "&::-webkit-scrollbar": {
+    width: "0px",
+    background: "transparent",
+  },
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
+});
 
-export const NavBar = () => {
+// 9. Define el componente NavBar.
+export const NavBar = ({ sidebarVisible, sidebarStatus, closeSidebar }) => {
+  // Determina la clase CSS para la barra lateral (sidebar) en función de su visibilidad y estado
+  const className = sidebarVisible
+    ? sidebarStatus === "mobile-active"
+      ? "pc-sidebar pc-trigger mob-sidebar-active"
+      : "pc-sidebar pc-trigger"
+    : "pc-sidebar pc-sidebar-hide";
+
+  // Obtiene la función dispatch del hook useDispatch de Redux
+//   const dispatch = useDispatch();
+  //obtenesmos la informacion del usuario
+//   const { name_user, rol_user } = useSelector((state) => state.auth);
+
+  // Utiliza el hook useEffect para manejar clics fuera de la barra lateral
+  useEffect(() => {
+    // Función para manejar los clics fuera de la barra lateral
+    const handleOutsideClick = (event) => {
+      const nav = document.querySelector(".pc-sidebar");
+      // Verifica si el clic se hizo fuera de la barra lateral y si la barra lateral está visible en modo móvil
+      if (
+        nav &&
+        !nav.contains(event.target) &&
+        sidebarVisible &&
+        (sidebarStatus === "mobile-active" || window.innerWidth <= 1024)
+      ) {
+        // Llama a la función closeSidebar para cerrar la barra lateral
+        closeSidebar();
+      }
+    };
+
+    // Agrega un event listener para manejar los clics fuera de la barra lateral
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    // Elimina el event listener cuando el componente se desmonta o cambian las dependencias
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [sidebarVisible, sidebarStatus, closeSidebar]); // Dependencias del useEffect
+
+  // Función para manejar el evento de cierre de sesión
+  const onLogout = () => {
+    // Despacha la acción startLogout para iniciar el proceso de cierre de sesión
+    // dispatch(startLogout());
+  };
+    // 5. Renderiza el componente NavBar.
+    
+  const rol_user = 1;
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      {/* 15. Loader que se muestra mientras se carga la página */}
+      <div className="loader-bg">
+        <div className="loader-track">
+          <div className="loader-fill"></div>
+        </div>
+      </div>
+
+      {/* 16. Drawer que contiene el menú lateral */}
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={sidebarVisible}
+        onClose={closeSidebar}
+        className={className}
+        PaperProps={{ className: className }}
+      >
+        <DrawerHeader>
+          <a href="" className="b-brand text-dark">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <img
+                src="/assets/logo2.jpg"
+                style={{ width: "165px", height: "60px" }}
+                alt="StudioCinemas"
+                className="logo-lg"
+              />
+            </div>
+          </a>
+        </DrawerHeader>
+        <Divider />
+        <NavbarContent>
+          <ul className="pc-navbar" style={{ display: "block" }}>
+            <li className="pc-item pc-caption">
+              <label>Menú Principal</label>
+            </li>
+            <li className="pc-item">
+              <NavLink to="/Movies/list" className="pc-link active">
+                <span className="pc-micon">
+                  <i className="ti ti-movie"></i>
+                </span>
+                <span className="pc-mtext">Películas</span>
+              </NavLink>
+            </li>
+            {rol_user == 1 ? (
+              <>
+                <li className="pc-item pc-caption">
+                  <label>SUPERADMINISTRADOR</label>
+                </li>
+                <li className="pc-item">
+                  <NavLink to="/Bugs/edit" className="pc-link">
+                    <span className="pc-micon">
+                      <i className="ph-duotone ph-user-list"></i>
+                    </span>
+                    <span className="pc-mtext">Usuarios</span>
+                  </NavLink>
+                </li>
+
+                <li className="pc-item">
+                  <NavLink to="/Bugs/list" className="pc-link">
+                    <span className="pc-micon">
+                      <i className="ph-duotone ph-bug"></i>
+                    </span>
+                    <span className="pc-mtext">Control de Bug</span>
+                  </NavLink>
+                </li>
+              </>
+            ) : null}
+          </ul>
+          <div className="card pc-user-card">
+            <div className="card-body">
+              <div className="d-flex align-items-center">
+                <div className="avtar avtar-s btn-light-success">
+                  <i className="ph-duotone ph-buildings f-22"></i>
+                </div>
+                <div className="flex-grow-1 ms-3 me-2">
+                  <h6 className="mb-0">name_use</h6>
+                  <small>Administrator</small>
+                </div>
+                <div className="dropdown">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-link-secondary avtar arrow-none dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    data-bs-offset="0,20"
+                  >
+                    <i className="ph-duotone ph-windows-logo"></i>
+                  </a>
+                  <div className="dropdown-menu">
+                    <ul>
+                      <li>
+                        <a className="pc-user-links">
+                          <i className="ph-duotone ph-user"></i>
+                          <span>My Account</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a className="pc-user-links">
+                          <i className="ph-duotone ph-gear"></i>
+                          <span>Settings</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a className="pc-user-links">
+                          <i className="ph-duotone ph-lock-key"></i>
+                          <span>Lock Screen</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a onClick={onLogout} className="pc-user-links">
+                          <i className="ph-duotone ph-power"></i>
+                          <span>Logout</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </NavbarContent>
+      </Drawer>
+    </>
+  );
+};
