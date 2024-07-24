73195b7ce5d88a7403748165ffd75b6bef53c7be
roccacr <soporte@roccacr.com>
2024-07-24 12:18:22 -0600

    feat: Update TableroHome component with new card layout

--- src/app/components/TableroHome/TableroHome.jsx
+++ src/app/components/TableroHome/TableroHome.jsx
@@ -2,11 +2,24 @@
 
 
 
-export const TableroHome = () => {
+export const TableroHome = ({image, icons, nombre, cantidad}) => {
   return (
-      <div className="pc-container">
-          <div className="pc-content">
-              Hola
+      <div className="col-md-12 col-xxl-4">
+          <div className="card statistics-card-1">
+              <div className="card-body">
+                  <img src={image} alt="img" className="img-fluid img-bg" />
+                  <div className="d-flex align-items-center">
+                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
+                          <i className={icons}></i>
+                      </div>
+                      <div>
+                          <p className="text-muted mb-0">{nombre}</p>
+                          <div className="d-flex align-items-end">
+                              <h2 className="mb-0 f-w-500">{cantidad}</h2>
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </div>
       </div>
   );
--- src/app/pages/home/AppPage.jsx
+++ src/app/pages/home/AppPage.jsx
@@ -1,126 +1,29 @@
 import { TableroHome } from "../../components/TableroHome/TableroHome";
-import { AppLayout } from "../../layout/AppLayout"
+import { AppLayout } from "../../layout/AppLayout";
 
+// Define an array of dashboard items
+const dashboardItems = [
+    { image: "1.svg", icon: "ti ti-user", name: "LEADS NUEVOS", quantity: 50 },
+    { image: "2.svg", icon: "ti ti-user-x", name: "LEADS REQUIEREN ATENCIÓN", quantity: 250 },
+    { image: "3.svg", icon: "ti ti-calendar", name: "EVENTOS PARA HOY", quantity: 1 },
+    { image: "1.svg", icon: "ti ti-trending-up", name: "OPORTUNIDADES", quantity: 20 },
+    { image: "2.svg", icon: "ti ti-download", name: "ORDENES DE VENTA", quantity: 5 },
+    { image: "3.svg", icon: "ti ti-download", name: "CONTRATO FIRMADO", quantity: 3 },
+];
 
 export const AppPage = () => {
-  return (
-      <AppLayout>
-          <div className="pc-container">
-              <div className="pc-content">
-                  <div className="row">
-                      <div className="col-md-12 col-xxl-4">
-                          <div className="card statistics-card-1">
-                              <div className="card-body">
-                                  <img src="../../../../public/assets/panel/1.svg" alt="img" className="img-fluid img-bg" />
-                                  <div className="d-flex align-items-center">
-                                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
-                                          <i className="ti ti-users"></i>
-                                      </div>
-                                      <div>
-                                          <p className="text-muted mb-0">LEADS NUEVOS</p>
-                                          <div className="d-flex align-items-end">
-                                              <h2 className="mb-0 f-w-500">50</h2>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="col-md-12 col-xxl-4">
-                          <div className="card statistics-card-1">
-                              <div className="card-body">
-                                  <img src="../../../../public/assets/panel/2.svg" alt="img" className="img-fluid img-bg" />
-                                  <div className="d-flex align-items-center">
-                                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
-                                          <i className="ti ti-user-x"></i>
-                                      </div>
-                                      <div>
-                                          <p className="text-muted mb-0">LEADS REQUIEREN ATENCIÓN</p>
-                                          <div className="d-flex align-items-end">
-                                              <h2 className="mb-0 f-w-500">250</h2>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="col-md-12 col-xxl-4">
-                          <div className="card statistics-card-1">
-                              <div className="card-body">
-                                  <img src="../../../../public/assets/panel/3.svg" alt="img" className="img-fluid img-bg" />
-                                  <div className="d-flex align-items-center">
-                                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
-                                          <i className="ti ti-calendar"></i>
-                                      </div>
-                                      <div>
-                                          <p className="text-muted mb-0">EVENTOS PARA HOY</p>
-                                          <div className="d-flex align-items-end">
-                                              <h2 className="mb-0 f-w-500">2</h2>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="row">
-                      <div className="col-md-12 col-xxl-4">
-                          <div className="card statistics-card-1">
-                              <div className="card-body">
-                                  <img src="../../../../public/assets/panel/1.svg" alt="img" className="img-fluid img-bg" />
-                                  <div className="d-flex align-items-center">
-                                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
-                                          <i className="ti ti-trending-up"></i>
-                                      </div>
-                                      <div>
-                                          <p className="text-muted mb-0">OPORTUINDADES</p>
-                                          <div className="d-flex align-items-end">
-                                              <h2 className="mb-0 f-w-500">50</h2>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="col-md-12 col-xxl-4">
-                          <div className="card statistics-card-1">
-                              <div className="card-body">
-                                  <img src="../../../../public/assets/panel/2.svg" alt="img" className="img-fluid img-bg" />
-                                  <div className="d-flex align-items-center">
-                                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
-                                          <i className="ti ti-users"></i>
-                                      </div>
-                                      <div>
-                                          <p className="text-muted mb-0">ORDENES DE VENTA</p>
-                                          <div className="d-flex align-items-end">
-                                              <h2 className="mb-0 f-w-500">250</h2>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="col-md-12 col-xxl-4">
-                          <div className="card statistics-card-1">
-                              <div className="card-body">
-                                  <img src="../../../../public/assets/panel/3.svg" alt="img" className="img-fluid img-bg" />
-                                  <div className="d-flex align-items-center">
-                                      <div className="avtar" style={{ backgroundColor: "#000000", color: "#FFFFFF", marginRight: "1rem" }}>
-                                          <i className="ti ti-users"></i>
-                                      </div>
-                                      <div>
-                                          <p className="text-muted mb-0">CONTRATO FIRMADO</p>
-                                          <div className="d-flex align-items-end">
-                                              <h2 className="mb-0 f-w-500">2</h2>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </AppLayout>
-  );
-}
+    return (
+        <AppLayout>
+            <div className="pc-container">
+                <div className="pc-content">
+                    <div className="row">
+                        {/* Map through dashboardItems to render TableroHome components */}
+                        {dashboardItems.map((item, index) => (
+                            <TableroHome key={index} image={`/assets/panel/${item.image}`} icons={item.icon} nombre={item.name} cantidad={item.quantity} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </AppLayout>
+    );
+};
