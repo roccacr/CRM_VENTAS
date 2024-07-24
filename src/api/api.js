32f4dc0ac48d147a340b00fb601d4735c9c1a49f
roccacr <soporte@roccacr.com>
2024-07-24 09:26:55 -0600

    RZ feat: Update API URLs to use HTTPS for local development

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,15 @@ import axios from "axios";
 
 /********************************************** BASE API URL DEFINITION **********************************************/
 // Define la URL base de la API
-const apiUrl = window.location.hostname === "localhost" ? "http://localhost:7000/api/v2.0/" : "http://e050-186-4-2-178.ngrok-free.app/api/v2.0/";
+const apiUrl = window.location.hostname === "localhost" ? "http://localhost:7000/api/v2.0/" : "https://027b-186-4-2-178.ngrok-free.app/api/v2.0/";
+
+console.log("🚀 -------------------------------------🚀");
+console.log("🚀 ~ file: api.js:7 ~ apiUrl:", apiUrl);
+console.log("🚀 -------------------------------------🚀");
+
 
 // Define la URL base de la API para imágenes
-const apiUrlImg = window.location.hostname === "localhost" ? "http://localhost:7000/api/v2.0/" : "http://e050-186-4-2-178.ngrok-free.app/api/v2.0/";
+const apiUrlImg = window.location.hostname === "localhost" ? "http://localhost:7000/api/v2.0/" : "https://027b-186-4-2-178.ngrok-free.app/api/v2.0/";
 
 
 
