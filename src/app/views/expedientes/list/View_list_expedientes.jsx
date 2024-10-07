734fcb34ae3fb33c1ffb20b660ce37d086367679
Robertoart13 <carlosroart13@gmail.com>
2024-10-07 11:54:48 -0600

    Refactor ViewListExpedientes component to remove unnecessary column

--- src/app/views/expedientes/list/View_list_expedientes.jsx
+++ src/app/views/expedientes/list/View_list_expedientes.jsx
@@ -205,7 +205,6 @@ export default function ViewListExpedientes() {
                                             <td>{row.areaTotalM2_exp}</td>
                                             <td>{row.m2Habitables_exp}</td>
                                             <td>{row.loteM2_exp}</td>
-
                                             <td>{row.areaDeParqueoAprox}</td>
                                             <td>{row.areaDeBodegaM2_exp}</td>
                                             <td>{row.areaDeMezzanieM2_exp}</td>
