6d59b23153769ed9444710a4d18b222a411b6de1
Robertoart13 <carlosroart13@gmail.com>
2024-10-25 11:10:24 -0600

    Refactor code to add PerfilUsuario component in LeadsPage and update filter option in View_list_leads_Stragglers, View_list_leads_attention, and View_total_leads components

--- src/app/views/leads/perfil/InfromacionCompleta.jsx
+++ src/app/views/leads/perfil/InfromacionCompleta.jsx
@@ -1,6 +1,3 @@
-
-
-
 export const InfromacionCompleta = ({ leadDetails }) => {
     return (
         <>
@@ -19,23 +16,11 @@ export const InfromacionCompleta = ({ leadDetails }) => {
                 </div>
                 <div className="card-body">
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item px-0 pt-0">
+                        <li className="list-group-item px-0">
                             <div className="row">
                                 <div className="col-md-6">
                                     <p className="mb-1 text-muted">Nombre Completo</p>
                                     <p className="mb-0">{leadDetails.nombre_lead}</p>
-                                </div>
-                                <div className="col-md-6">
-                                    <p className="mb-1 text-muted">Correo</p>
-                                    <p className="mb-0">{leadDetails.email_lead}</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="list-group-item px-0">
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <p className="mb-1 text-muted">Telefono</p>
-                                    <p className="mb-0">{leadDetails.telefono_lead}</p>
                                 </div>
                                 <div className="col-md-6">
                                     <p className="mb-1 text-muted">Proyecto</p>
@@ -47,17 +32,55 @@ export const InfromacionCompleta = ({ leadDetails }) => {
                             <div className="row">
                                 <div className="col-md-6">
                                     <p className="mb-1 text-muted">Campaña</p>
-                                    <p className="mb-0">{leadDetails.proyecto_lead}</p>
+                                    <p className="mb-0">{leadDetails.campana_lead}</p>
                                 </div>
                                 <div className="col-md-6">
-                                    <p className="mb-1 text-muted">Zip Code</p>
-                                    <p className="mb-0">956 754</p>
+                                    <p className="mb-1 text-muted">Comentario</p>
+                                    <p className="mb-0">{leadDetails.comentario_lead}</p>
                                 </div>
                             </div>
                         </li>
-                        <li className="list-group-item px-0 pb-0">
-                            <p className="mb-1 text-muted">Address</p>
-                            <p className="mb-0">Street 110-B Kalians Bag, Dewan, M.P. New York</p>
+
+                        <li className="list-group-item px-0">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <p className="mb-1 text-muted">Estado Interesado</p>
+                                    <p className="mb-0">{leadDetails.estadointeresado_lead}</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <p className="mb-1 text-muted">Correo</p>
+                                    <p className="mb-0">{leadDetails.email_lead}</p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="list-group-item px-0">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <p className="mb-1 text-muted">Teléfono</p>
+                                    <p className="mb-0">{leadDetails.telefono_lead}</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <p className="mb-1 text-muted">Subsidiaria</p>
+                                    <p className="mb-0">{leadDetails.subsidiaria_lead}</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <hr/>
+                    <h5>Infirmacion extra</h5>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item px-0">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <p className="mb-1 text-muted">Hijos</p>
+                                    <p className="mb-0">2</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <p className="mb-1 text-muted">Profescion</p>
+                                    <p className="mb-0">inge</p>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
