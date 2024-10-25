e53fc87f7f6962097900c6825206821fd2416cd5
Robertoart13 <carlosroart13@gmail.com>
2024-10-25 11:59:56 -0600

    Refactor config.js: Remove duplicate database configuration

--- src/config/config.js
+++ src/config/config.js
@@ -8,11 +8,6 @@ const dbDevelopment = process.env.DB_NAME_DEVELOPMENT || "Valor por defecto si e
 const dbProduction = process.env.DB_NAME_PRODUCTION || "Valor por defecto si es null";
 
 
-const hostProduction = process.env.OAUTH_NETSUITE_CONSUMER_KEY || "Valor por defecto si es null";
-const userProduction = process.env.DB_USER_PRODUCTION || "Valor por defecto si es null";
-const passProduction = process.env.DB_PASS_PRODUCTION || "Valor por defecto si es null";
-const dbDevelopment = process.env.DB_NAME_DEVELOPMENT || "Valor por defecto si es null";
-const dbProduction = process.env.DB_NAME_PRODUCTION || "Valor por defecto si es null";
 
 const jwtSecret = process.env.JWT_SECRET;
 
