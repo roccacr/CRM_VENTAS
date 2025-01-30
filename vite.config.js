1fec7ce5c98bc7262f69dbfd345af47934f65ad9
Robertoart13 <carlosroart13@gmail.com>
2025-01-30 17:20:14 -0600

    xasas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+    base: "/",
+    plugins: [react()],
+    build: {
+        sourcemap: true, // Habilitar source maps para la compilación en producción
+        outDir: "dist", // Directorio de salida para los archivos de producción
+        manifest: true, // Generar un manifiesto para integraciones
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/[name].[hash].js",
+                chunkFileNames: "assets/[name].[hash].js",
+                assetFileNames: "assets/[name].[hash].[ext]",
+            },
+        },
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: "globalThis", // Definir 'global' para compatibilidad con Node.js
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true, // Polyfill para buffer
+                    crypto: true, // Polyfill para crypto
+                }),
+            ],
+        },
+    },
+    resolve: {
+        alias: {
+            crypto: "crypto-browserify", // Alias para utilizar 'crypto-browserify' en lugar de la API de navegador
+        },
+    },
 });
