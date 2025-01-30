609b30f474fbb1614f6c951521294b6f64ec6f21
Robertoart13 <carlosroart13@gmail.com>
2025-01-30 17:29:29 -0600

    Refactor package.json to remove crypto-browserify package

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
-    plugins: [react()],
-    build: {
-        sourcemap: true, // Habilitar source maps para la compilación en producción
-        outDir: "dist", // Directorio de salida para los archivos de producción
-        manifest: true, // Generar un manifiesto para integraciones
-        rollupOptions: {
-            output: {
-                entryFileNames: "assets/[name].[hash].js",
-                chunkFileNames: "assets/[name].[hash].js",
-                assetFileNames: "assets/[name].[hash].[ext]",
-            },
-        },
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: "globalThis", // Definir 'global' para compatibilidad con Node.js
-            },
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true, // Polyfill para buffer
-                    crypto: true, // Polyfill para crypto
-                }),
-            ],
-        },
-    },
-    resolve: {
-        alias: {
-            crypto: "crypto-browserify", // Alias para utilizar 'crypto-browserify' en lugar de la API de navegador
-        },
-    },
+  plugins: [react()],
+  base: "/",
 });
