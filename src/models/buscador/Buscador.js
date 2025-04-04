66b2721430e8fe83e745859019acae01b8a58e6d
roccacr <soporte@roccacr.com>
2025-04-04 03:34:15 -0600

    Enhance buscador model to include role-based filtering in leads, oportunidades, and eventos queries, improving search result accuracy for admin users.

--- src/models/buscador/Buscador.js
+++ src/models/buscador/Buscador.js
@@ -12,13 +12,16 @@ buscador.getAll = async (dataParams) => {
 
     console.log(dataParams);
     
-    const { selectedOption, searchs } = dataParams;
+    const { selectedOption, searchs, idnetsuite_admin, rol_admin } = dataParams; 
 
     if (selectedOption === "leads") {
         const query = `SELECT l.*, a.name_admin as nombre_admin FROM leads as l 
         INNER JOIN admins as a ON a.idnetsuite_admin = l.id_empleado_lead 
-        WHERE l.nombre_lead LIKE ?`;
-        const params = [`%${searchs }%`];
+        WHERE l.nombre_lead LIKE ? ${rol_admin === '2' ? 'AND l.id_empleado_lead= ?' : ''}`;
+        const params = [`%${searchs}%`];
+        if (rol_admin === '2') {
+            params.push(idnetsuite_admin);
+        }
         const result = await executeQuery(query, params, dataParams.database);
         return result;
     }
@@ -27,8 +30,11 @@ buscador.getAll = async (dataParams) => {
         const query = `SELECT p.*,a.name_admin as employee_oport, l.nombre_lead as entity_oport  FROM oportunidades AS p 
         INNER JOIN admins as a ON a.idnetsuite_admin = p.employee_oport 
         INNER JOIN leads as l ON l.idinterno_lead  = p.entity_oport 
-        WHERE p.tranid_oport LIKE ?`;
+        WHERE p.tranid_oport LIKE ? ${rol_admin === '2' ? 'AND p.employee_oport  = ?' : ''}`;
         const params = [`%${searchs}%`];
+        if (rol_admin === '2') {
+            params.push(idnetsuite_admin);
+        }
         const result = await executeQuery(query, params, dataParams.database);
         return result;
     }
@@ -37,8 +43,11 @@ buscador.getAll = async (dataParams) => {
         const query = `SELECT e.*, a.name_admin AS employee_evento, COALESCE(l.nombre_lead, 'No aplica') AS nombre_lead FROM calendars AS e 
         INNER JOIN admins AS a ON a.idnetsuite_admin = e.id_admin 
         LEFT JOIN leads AS l ON l.idinterno_lead = e.id_lead
-        WHERE e.nombre_calendar LIKE ?`;
+        WHERE e.nombre_calendar LIKE ? ${rol_admin === '2' ? 'AND e.id_admin  = ?' : ''}`;
         const params = [`%${searchs}%`];
+        if (rol_admin === '2') {
+            params.push(idnetsuite_admin);
+        }
         const result = await executeQuery(query, params, dataParams.database);
         return result;
     }
