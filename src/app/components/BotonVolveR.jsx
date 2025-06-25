26f4476e2f5c4a5d22573def9da9ff3e7e5eadd2
Robertoart13 <carlosroart13@gmail.com>
2025-06-25 10:54:25 -0600

    Refactor BotonVolveR: actualizar la función de retorno para usar la API de historial en lugar de forzar la recarga de la página.

--- src/app/components/BotonVolveR.jsx
+++ src/app/components/BotonVolveR.jsx
@@ -1,14 +1,14 @@
 export const BotonVolveR = () => {
     const handleVolverClick = () => {
-        // Forzar recarga completa de la página al volver
-        window.location.href = document.referrer;
+        // Usar la API de historial para volver a la página anterior
+        window.history.back();
     };
 
     return (
         <div className="col-sm-6 ms-auto my-1">
             <ul className="list-inline footer-link mb-0 justify-content-sm-end d-flex">
                 <button className="btn btn-dark" onClick={handleVolverClick}>
-                  Vista anterior
+                    Vista anterior
                 </button>
             </ul>
         </div>
