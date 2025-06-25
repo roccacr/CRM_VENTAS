fc46207783dcdd4e5cd81778f2bffbfa0e0c0c10
Robertoart13 <carlosroart13@gmail.com>
2025-06-25 11:10:49 -0600

    Refactor BotonVolveR: renombrar la función de manejo de clic para mayor claridad y consistencia en el código.

--- src/app/components/BotonVolveR.jsx
+++ src/app/components/BotonVolveR.jsx
@@ -1,5 +1,5 @@
 export const BotonVolveR = () => {
-    const handleVolverClick = () => {
+    const handleVolverClickPagina = () => {
         // Usar la API de historial para volver a la página anterior
         window.history.back();
     };
@@ -7,7 +7,7 @@ export const BotonVolveR = () => {
     return (
         <div className="col-sm-6 ms-auto my-1">
             <ul className="list-inline footer-link mb-0 justify-content-sm-end d-flex">
-                <button className="btn btn-dark" onClick={handleVolverClick}>
+                <button className="btn btn-dark" onClick={handleVolverClickPagina}>
                     Vista anterior
                 </button>
             </ul>
