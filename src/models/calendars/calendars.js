971b32029619aeffad801082d59a0239dc75c76d
Robertoart13 <carlosroart13@gmail.com>
2025-07-22 13:38:12 -0600

    Add copiaJefe parameter to createEvent and editEvent methods in calendars model for improved event management, allowing notification to sales supervisor.

--- src/models/calendars/calendars.js
+++ src/models/calendars/calendars.js
@@ -38,7 +38,8 @@ calendars.createEvent = (dataParams) =>
             dataParams.citaValue, // Color visual que se asigna al evento campo citas_chek
             dataParams.citaValue, // Color visual que se asigna al evento campo masDeUnaCita_calendar
             dataParams.id_proyecto, // ID del proyecto relacionado con el evento
-            dataParams.nombre_proyecto, // Nombre del proyecto relacionado con el evento
+            dataParams.nombre_proyecto, // Nombre del proyecto relacionado con el evento    
+            dataParams.copiaJefe, // Si es true, copia al jefe de ventas (supervisor), en este caso a Fabián Mata.
         ], // Parámetros que identifican el rol y el ID del administrador.
         dataParams.database, // Nombre de la base de datos a utilizar.
     );
@@ -112,6 +113,7 @@ calendars.editEvent = (dataParams) => {
             dataParams.citaValue, // Color visual que se asigna al evento campo masDeUnaCita_calendar
             dataParams.id_proyecto, // ID del proyecto relacionado con el evento
             dataParams.nombre_proyecto, // Nombre del proyecto relacionado con el evento
+            dataParams.copiaJefe, // Si es true, copia al jefe de ventas (supervisor), en este caso a Fabián Mata.
         ], // Parámetros que identifican el rol y el ID del administrador.
         dataParams.database, // Nombre de la base de datos a utilizar.
     );
