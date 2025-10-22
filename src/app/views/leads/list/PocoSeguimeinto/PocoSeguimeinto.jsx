99cf878791f274f0dfbc0c7d25abd305a5e3277a
Roberto Zuniga Altamirano <carlosroart13@gmail.com>
2025-10-22 16:17:33 -0600

    Update PocoSeguimeinto component to include column 7 in data table configuration, enhancing data visibility and user interaction.

--- src/app/views/leads/list/PocoSeguimeinto/PocoSeguimeinto.jsx
+++ src/app/views/leads/list/PocoSeguimeinto/PocoSeguimeinto.jsx
@@ -166,7 +166,7 @@ const getDataTableConfig = (tableElement, inputStartDate, inputEndDate, filterOp
             searching: true,
          },
          viewTotal: true,
-         columns: [0, 1, 3, 4, 5, 6, 11],
+         columns: [0, 1, 3, 4, 5, 6,7],
       },
       processing: true,
       dom: "lPBfrtip",
