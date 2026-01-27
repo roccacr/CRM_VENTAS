0ff30e3c85e30c85a5315672f599190918852dab
Roberto Zuniga Altamirano <carlosroart13@gmail.com>
2026-01-27 11:48:52 -0600

    Update allowedOrigins for CORS configuration to include new localhost port
    - Modified the allowedOrigins array in AppCrmVentas.js to add "http://localhost:5174", enhancing local development support while maintaining existing production and test origins.

--- src/AppCrmVentas.js
+++ src/AppCrmVentas.js
@@ -36,7 +36,7 @@ app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 
 // Lista de orígenes permitidos para CORS
 // Se define como una constante para mayor seguridad y claridad
-const allowedOrigins = ["http://localhost:5173", "https://crm.roccacr.com", "https://test.roccacr.com", "https://4552704-sb1.app.netsuite.com", "https://crmtest.roccacr.com"];
+const allowedOrigins = ["http://localhost:5174","http://localhost:5173", "https://crm.roccacr.com", "https://test.roccacr.com", "https://4552704-sb1.app.netsuite.com", "https://crmtest.roccacr.com"];
 
 // Configuración de CORS
 app.use(
